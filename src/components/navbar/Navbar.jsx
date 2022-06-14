486581d2a76a800325b6c81dea300fbd8dc05896
Michael Chilaka <obichilaka@gmail.com>
2022-06-14 13:41:26 +0100

    added assets folder

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+
+import MolloLogo from "../../assets/mollo.png"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="mollo__navbar">
+      <div className="mollo__navbar-links">
+        <div className="mollo__navbar-links__logo">
+          <img src={MolloLogo} alt="mollo-logo" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
