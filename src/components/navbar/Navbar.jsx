921cfc0ad8ce1e7c0648fdcbe4af01230cb600db
Michael Chilaka <obichilaka@gmail.com>
2022-06-14 23:42:32 +0100

    updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,6 +11,23 @@ const Navbar = () => {
         <div className="mollo__navbar-links__logo">
           <img src={MolloLogo} alt="mollo-logo" />
         </div>
+        <div className="mollo__navbar-links__container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="wpgt3">What is Mollo</a>
+          </p>
+          <p>
+            <a href="possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
       </div>
     </div>
   );
