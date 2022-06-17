0232c70610836b9ae4bf78a134d8430868ea659a
Michael Chilaka <obichilaka@gmail.com>
2022-06-17 12:19:24 +0100

    updates on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 
 import MolloLogo from "../../assets/mollo.png";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="mollo__navbar">
       <div className="mollo__navbar-links">
@@ -31,6 +33,46 @@ const Navbar = () => {
         <div className="mollo__navbar-sign">
           <p>Sign in</p>
           <button type="button">Sign up</button>
+        </div>
+        <div className="mollo__navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="mollo__navbar-menu__container scale-up-center">
+              <div className="mollo__navbar-menu__container-links">
+                <p>
+                  <a href="#home">Home</a>
+                </p>
+                <p>
+                  <a href="#wgpt3">What is Mollo?</a>
+                </p>
+                <p>
+                  <a href="#possibility">Open AI</a>
+                </p>
+                <p>
+                  <a href="#features">Case Studies</a>
+                </p>
+                <p>
+                  <a href="#blog">Library</a>
+                </p>
+              </div>
+              <div className="mollo__navbar-menu__container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
